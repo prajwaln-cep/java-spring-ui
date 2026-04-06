f3783fc56f60716baaa842c5f169cb23e54161df
Prajwal N <prajwal.n@ceptes.com>
2026-04-06 11:10:19 +0530

    Updated employee.ts to avoid the erros

--- angular8-springboot-client/src/app/employee.ts
+++ angular8-springboot-client/src/app/employee.ts
@@ -1,7 +1,7 @@
 export class Employee {
-  id: number;
-  firstName: string;
-  lastName: string;
-  emailId: string;
-  active: boolean;
+  id!: number;
+  firstName!: string;
+  lastName!: string;
+  emailId!: string;
+  active!: boolean;
 }
